888ba1c2abde03d49f25b5056e7e26c3cd5586e5
azero <azer0@users.noreply.github.com>
2020-01-25 14:24:48 -0500

    Update to trav leecher
    Will now leave trav when leader is not in trav or kurast.

--- d2bs/kolbot/libs/bots/TravincalLeech.js
+++ d2bs/kolbot/libs/bots/TravincalLeech.js
@@ -1,23 +1,41 @@
 /**
 *   @filename   TravincalLeech.js
-*   @author	 ToS/XxXGoD/YGM
+*   @author	 ToS/XxXGoD/YGM/azero
 *   @desc	   Travinical Leech
 */
 
 function TravincalLeech() {
+	
+	this.findLeader = function (name) {
+		var party = getParty(name);
+
+		if (party) {
+			return party;
+		}
+
+		return false;
+	};
+	
 	Town.goToTown(3);
+	Town.doChores();
 	Town.move("portalspot");
 
 	while (!Misc.inMyParty(Config.Leader)) {
 		delay(500);
 	}
+	
+	var leader = this.findLeader(Config.Leader);
 
 	while (Misc.inMyParty(Config.Leader)) {
 		if (me.inTown && Pather.getPortal(83, Config.Leader)) {
 			Pather.usePortal(83, Config.Leader);
 			Town.getCorpse();
 		}
-
+		
+		if(me.area === 83 && leader.area !== 83 && leader.area !== 75) {
+			break;
+		}
+		
 		if (me.mode === 17) {
 			me.revive();
 
