1862392ee9b8ed29f2a2fd5bc8d886dd30e7bb2f
mf022 <41995123+mf022@users.noreply.github.com>
2020-03-05 13:41:58 +0200

    Update AutoMule.js

--- d2bs/kolbot/libs/AutoMule.js
+++ d2bs/kolbot/libs/AutoMule.js
@@ -108,6 +108,10 @@ var AutoMule = {
 		if (info && info.hasOwnProperty("muleInfo")) {
 			items = this.getMuleItems();
 
+			if (info.muleInfo.hasOwnProperty("usedInventoryTrigger") && Storage.Inventory.UsedSpacePercent() >= info.muleInfo.usedInventoryTrigger && Config.LowGold > 0) {
+				Town.doChores();
+			}
+
 			if (info.muleInfo.hasOwnProperty("usedStashTrigger") && info.muleInfo.hasOwnProperty("usedInventoryTrigger") &&
 					Storage.Inventory.UsedSpacePercent() >= info.muleInfo.usedInventoryTrigger && Storage.Stash.UsedSpacePercent() >= info.muleInfo.usedStashTrigger &&
 						items.length > 0) {
