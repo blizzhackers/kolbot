4b76a4c9448526618033f8912d8189a2ee6d33e5
casualBotter <60308670+theBGuy@users.noreply.github.com>
2021-06-28 17:38:37 -0400

    Revert "Update D2BotMap"
    This reverts commit 5f27f4763a1bc4d67149d14cc9a79c6935a2f8cb.

--- d2bs/kolbot/libs/config/_CustomConfig.js
+++ d2bs/kolbot/libs/config/_CustomConfig.js
@@ -1,17 +1,9 @@
-/* Format:
-	var CustomConfig = {
+var CustomConfig = {
+	/* Format:
 		"Config_Filename_Without_Extension": ["array", "of", "profiles"]
 
 		Multiple entries are separated by commas
+	*/
 
 
-	};
-*/
-
-var CustomConfig;
-
-if (getScript("D2BotMap.dbj")) {
-	CustomConfig = {
-		"ManualPlay": me.profile,
-	};
-}
+};
