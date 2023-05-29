0fd8d949e6777e5ad5418520bcb0b2275bb3b1e3
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-05-29 11:55:47 -0400

    Update Skill.d.ts
    - update the type defs

--- d2bs/kolbot/sdk/types/Skill.d.ts
+++ d2bs/kolbot/sdk/types/Skill.d.ts
@@ -1,44 +1,31 @@
 export {};
 declare global {
-	function SkillData(skillId: number): void;
-	class SkillData {
-			constructor(skillId: number);
-			hardpoints: boolean;
-			checked: boolean;
-			manaCost: any;
-			condition: Function;
-			have(skill: number): any;
-	}
-	namespace Skill {
-		let usePvpRange: boolean;
-		const manaCostList: object;
-		const needFloor: number[];
-		const missileSkills: number[];
-		const charges: any[];
-		const haveTK: boolean;
-
-		namespace skills {
-			const all: {
-				[x: number]: SkillData;
-			};
-			let initialized: boolean;
-			function init(): void;
-			function have(skill: number): boolean;
-			function reset(): void;
-		}
-		function init(): void;
-		function canUse(skillId: number): boolean;
-		function getDuration(skillId: number): number;
-		function getMaxSummonCount(skillId: number): number;
-		function getRange(skillId: any): number;
-		function getHand(skillId: any): number;
-		function cast(skillId: number, hand?: number, x?: number, y?: number, item?: ItemUnit | undefined): boolean;
-		function cast(skillId: number, hand?: number, unit?: Unit): boolean;
-		function setSkill(skillId: any, hand?: any, item?: any): boolean;
-		function isTimed(skillId: any): boolean;
-		function wereFormCheck(skillId: any): boolean;
-		function townSkill(skillId: any): boolean;
-		function getManaCost(skillId: any): number;
-		function useTK(unit: Unit): boolean;
-	}
+  namespace Skill {
+    let usePvpRange: boolean;
+    const manaCostList: object;
+    const needFloor: number[];
+    const missileSkills: number[];
+    const charges: any[];
+    
+    function getClassSkillRange(classid?: number): [number, number];
+    function init(): void;
+    function canUse(skillId: number): boolean;
+    function getDuration(skillId: number): number;
+    function getMaxSummonCount(skillId: number): number;
+    function getRange(skillId: number): number;
+    function getAoE(skillId: number): number;
+    function getHand(skillId: number): number;
+    function getState(skillId: number): number;
+    function getManaCost(skillId: number): number;
+    function isTimed(skillId: number): boolean;
+    function townSkill(skillId: number): boolean;
+    function missileSkill(skillId: number): boolean;
+    function wereFormCheck(skillId: number): boolean;
+    function setSkill(skillId: number, hand?: number, item?: any): boolean;
+    function shapeShift(mode: number | string): boolean;
+    function unShift(): boolean;
+    function useTK(unit: Unit): boolean;
+    function cast(skillId: number, hand?: number, x?: number, y?: number, item?: ItemUnit | undefined): boolean;
+    function cast(skillId: number, hand?: number, unit?: Unit): boolean;
+  }
 }
