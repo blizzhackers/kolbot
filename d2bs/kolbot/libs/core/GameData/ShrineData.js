60c3f1144932197e62f45b8c5e405e38530d71de
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-05-29 00:50:23 -0400

    Update ShrineData.js
    - small refactor. Initialize `_shrines` map instead of setting each after creation

--- d2bs/kolbot/libs/core/GameData/ShrineData.js
+++ d2bs/kolbot/libs/core/GameData/ShrineData.js
@@ -12,49 +12,50 @@
       this.duration = duration || 0;
       this.regenTime = Time.minutes(regen) || Infinity;
     }
-    const shrineMap = new Map();
-    shrineMap.set(sdk.shrines.Refilling, new Shrine(0, 0, 2));
-    shrineMap.set(sdk.shrines.Health, new Shrine(0, 0, 5));
-    shrineMap.set(sdk.shrines.Mana, new Shrine(0, 0, 5));
-    shrineMap.set(sdk.shrines.HealthExchange, new Shrine());
-    shrineMap.set(sdk.shrines.ManaExchange, new Shrine());
-    shrineMap.set(sdk.shrines.Armor, new Shrine(sdk.states.ShrineArmor, 2400, 5));
-    shrineMap.set(sdk.shrines.Combat, new Shrine(sdk.states.ShrineCombat, 2400, 5));
-    shrineMap.set(sdk.shrines.ResistFire, new Shrine(sdk.states.ShrineResFire, 3600, 5));
-    shrineMap.set(sdk.shrines.ResistCold, new Shrine(sdk.states.ShrineResCold, 3600, 5));
-    shrineMap.set(sdk.shrines.ResistLightning, new Shrine(sdk.states.ShrineResLighting, 3600, 5));
-    shrineMap.set(sdk.shrines.ResistPoison, new Shrine(sdk.states.ShrineResPoison, 3600, 5));
-    shrineMap.set(sdk.shrines.Skill, new Shrine(sdk.states.ShrineSkill, 2400, 5));
-    shrineMap.set(sdk.shrines.ManaRecharge, new Shrine(sdk.states.ShrineManaRegen, 2400, 5));
-    shrineMap.set(sdk.shrines.Stamina, new Shrine(sdk.states.ShrineStamina, 4800, 5));
-    shrineMap.set(sdk.shrines.Experience, new Shrine(sdk.states.ShrineResCold, 3600));
-    shrineMap.set(sdk.shrines.Enirhs, new Shrine());
-    shrineMap.set(sdk.shrines.Portal, new Shrine());
-    shrineMap.set(sdk.shrines.Gem, new Shrine());
-    shrineMap.set(sdk.shrines.Fire, new Shrine());
-    shrineMap.set(sdk.shrines.Monster, new Shrine());
-    shrineMap.set(sdk.shrines.Exploding, new Shrine());
-    shrineMap.set(sdk.shrines.Poison, new Shrine());
+    const _shrines = new Map([
+      [sdk.shrines.Refilling, new Shrine(sdk.shrines.None, 0, 2)],
+      [sdk.shrines.Health, new Shrine(sdk.shrines.None, 0, 5)],
+      [sdk.shrines.Mana, new Shrine(sdk.shrines.None, 0, 5)],
+      [sdk.shrines.HealthExchange, new Shrine()],
+      [sdk.shrines.ManaExchange, new Shrine()],
+      [sdk.shrines.Armor, new Shrine(sdk.states.ShrineArmor, 2400, 5)],
+      [sdk.shrines.Combat, new Shrine(sdk.states.ShrineCombat, 2400, 5)],
+      [sdk.shrines.ResistFire, new Shrine(sdk.states.ShrineResFire, 3600, 5)],
+      [sdk.shrines.ResistCold, new Shrine(sdk.states.ShrineResCold, 3600, 5)],
+      [sdk.shrines.ResistLightning, new Shrine(sdk.states.ShrineResLighting, 3600, 5)],
+      [sdk.shrines.ResistPoison, new Shrine(sdk.states.ShrineResPoison, 3600, 5)],
+      [sdk.shrines.Skill, new Shrine(sdk.states.ShrineSkill, 2400, 5)],
+      [sdk.shrines.ManaRecharge, new Shrine(sdk.states.ShrineManaRegen, 2400, 5)],
+      [sdk.shrines.Stamina, new Shrine(sdk.states.ShrineStamina, 4800, 5)],
+      [sdk.shrines.Experience, new Shrine(sdk.states.ShrineResCold, 3600)],
+      [sdk.shrines.Enirhs, new Shrine()],
+      [sdk.shrines.Portal, new Shrine()],
+      [sdk.shrines.Gem, new Shrine()],
+      [sdk.shrines.Fire, new Shrine()],
+      [sdk.shrines.Monster, new Shrine()],
+      [sdk.shrines.Exploding, new Shrine()],
+      [sdk.shrines.Poison, new Shrine()],
+    ]);
     
     return {
       get: function (shrineType) {
-        return shrineMap.get(shrineType);
+        return _shrines.get(shrineType);
       },
 
       has: function (shrineType) {
-        return shrineMap.has(shrineType);
+        return _shrines.has(shrineType);
       },
 
       getState: function (shrineType) {
-        return shrineMap.get(shrineType).state || 0;
+        return _shrines.get(shrineType).state || 0;
       },
 
       getDuration: function (shrineType) {
-        return shrineMap.get(shrineType).duration || 0;
+        return _shrines.get(shrineType).duration || 0;
       },
 
       getRegenTime: function (shrineType) {
-        return shrineMap.get(shrineType).regenTime || Infinity;
+        return _shrines.get(shrineType).regenTime || Infinity;
       },
     };
   })();
