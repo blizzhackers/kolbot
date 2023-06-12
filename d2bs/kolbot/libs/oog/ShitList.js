252a20113c030654da9164d3b0a00e94e8b27d5f
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-06-12 00:16:59 -0400

    Update ShitList.js
    - some typedefs and fixed where the file was created at

--- d2bs/kolbot/libs/oog/ShitList.js
+++ d2bs/kolbot/libs/oog/ShitList.js
@@ -10,19 +10,38 @@ includeIfNotIncluded("oog/FileAction.js");
 
 const ShitList = {
   _default: {
+    /** @type {Array<string>} */
     shitlist: []
   },
+  _path: "logs/shitlist.json",
+  _list: new Set(),
+
+  /**
+   * @private
+   * @returns {{ shitlist: Array<string> }}}
+   */
   create: function () {
     let string = JSON.stringify(this._default);
+    FileAction.write(this._path, string);
 
-    FileAction.write("shitlist.json", string);
-
-    return obj;
+    return Object.assign({}, this._default);
   },
 
+  reset: function () {
+    let string = JSON.stringify(this._default);
+    FileAction.write(this._path, string);
+    this._list.clear();
+
+    return Object.assign({}, this._default);
+  },
+
+  /**
+   * @private
+   * @returns {{ shitlist: Array<string> }}}
+   */
   getObj: function () {
     let obj;
-    let string = FileAction.read("shitlist.json");
+    let string = FileAction.read(this._path);
 
     try {
       obj = JSON.parse(string);
@@ -36,24 +55,48 @@ const ShitList = {
 
     console.warn("Failed to read ShitList. Using null values");
 
-    return this._default;
+    return Object.assign({}, this._default);
   },
 
+  /** @param {Array<string>} name */
   read: function () {
-    !FileTools.exists("shitlist.json") && this.create();
-    
+    if (!FileTools.exists(this._path)) {
+      return this.create().shitlist;
+    }
     let obj = this.getObj();
-
+    if (!this._list.size) {
+      obj.shitlist.forEach(name => this._list.add(name));
+    }
     return obj.shitlist;
   },
 
+  /** @param {string} name */
   add: function (name) {
     let obj = this.getObj();
-
     obj.shitlist.push(name);
+    this._list.add(name);
 
     let string = JSON.stringify(obj);
 
-    FileAction.write("shitlist.json", string);
+    FileAction.write(this._path, string);
+  },
+
+  /** @param {string} name */
+  remove: function (name) {
+    let obj = this.getObj();
+    let index = obj.shitlist.indexOf(name);
+    if (index === -1) return false;
+    obj.shitlist.splice(index, 1);
+    this._list.delete(name);
+
+    let string = JSON.stringify(obj);
+
+    FileAction.write(this._path, string);
+    return true;
+  },
+
+  /** @param {string} name */
+  has: function (name) {
+    return this._list.has(name);
   }
 };
