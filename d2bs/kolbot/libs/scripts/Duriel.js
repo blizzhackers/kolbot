456a23dec8f0636eca7a0f6d4867ba76215690da
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-06-22 13:02:39 -0400

    Update Duriel.js
    - fix attempt to use lair entrance twice

--- d2bs/kolbot/libs/scripts/Duriel.js
+++ d2bs/kolbot/libs/scripts/Duriel.js
@@ -7,10 +7,14 @@
 
 function Duriel () {
   const killDuriel = function () {
-    let target = Misc.poll(() => Game.getMonster(sdk.monsters.Duriel), 1000, 200);
+    let target = Misc.poll(function () {
+      return Game.getMonster(sdk.monsters.Duriel);
+    }, 1000, 200);
     if (!target) throw new Error("Duriel not found.");
 
-    Config.MFLeader && Pather.makePortal() && say("kill " + sdk.monsters.Duriel);
+    if (Config.MFLeader && Pather.makePortal()) {
+      say("kill " + sdk.monsters.Duriel);
+    }
 
     for (let i = 0; i < 300 && target.attackable; i += 1) {
       ClassAttack.doAttack(target);
@@ -27,31 +31,47 @@ function Duriel () {
 
   Precast.doPrecast(true);
 
-  if (!Pather.moveToExit(getRoom().correcttomb, true)) throw new Error("Failed to move to Tal Rasha's Tomb");
-  if (!Pather.moveToPreset(me.area, sdk.unittype.Object, sdk.quest.chest.HoradricStaffHolder, -11, 3)) {
+  if (!Pather.moveToExit(getRoom().correcttomb, true)) {
+    throw new Error("Failed to move to Tal Rasha's Tomb");
+  }
+  /** @type {ObjectUnit} */
+  let lairEntrance = null;
+  if (!Pather.moveToPresetObject(me.area, sdk.quest.chest.HoradricStaffHolder,
+    { offX: -11, offY: 3, callback: function () {
+      lairEntrance = Game.getObject(sdk.objects.PortaltoDurielsLair);
+      return lairEntrance && lairEntrance.distance < 20;
+    } })) {
     throw new Error("Failed to move to Orifice");
   }
 
-  me.hardcore && !me.sorceress && Attack.clear(5);
-
-  let unit = Game.getObject(sdk.objects.PortaltoDurielsLair);
-
-  if (Skill.useTK(unit)) {
+  // me.hardcore && !me.sorceress && Attack.clear(5);
+  if (lairEntrance && Skill.useTK(lairEntrance)) {
+    if (lairEntrance.distance > 20) {
+      Attack.getIntoPosition(lairEntrance, 20, sdk.collision.LineOfSight);
+    }
     Misc.poll(function () {
-      Packet.telekinesis(unit) && delay(100);
+      Packet.telekinesis(lairEntrance) && delay(100);
       return me.inArea(sdk.areas.DurielsLair);
     }, 1000, 200);
   }
 
+  let [type, id, target] = [
+    sdk.unittype.Object, sdk.objects.PortaltoDurielsLair, sdk.areas.DurielsLair
+  ];
+
   if (!me.inArea(sdk.areas.DurielsLair)
-    && Pather.useUnit(sdk.unittype.Object, sdk.objects.PortaltoDurielsLair, sdk.areas.DurielsLair)) {
+    && !Pather.useUnit(type, id, target)) {
     Attack.clear(10);
-    Pather.useUnit(sdk.unittype.Object, sdk.objects.PortaltoDurielsLair, sdk.areas.DurielsLair);
+    Pather.useUnit(type, id, target);
   }
 
-  if (!me.inArea(sdk.areas.DurielsLair)) throw new Error("Failed to move to Duriel");
+  if (!me.inArea(sdk.areas.DurielsLair)) {
+    throw new Error("Failed to move to Duriel");
+  }
 
-  me.sorceress && me.classic ? killDuriel() : Attack.kill(sdk.monsters.Duriel);
+  me.sorceress && me.classic
+    ? killDuriel()
+    : Attack.kill(sdk.monsters.Duriel);
   Pickit.pickItems();
 
   return true;
