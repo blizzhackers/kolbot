730051bfb1f66948ab506f7d433f15adff05a21f
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-07-07 00:42:24 -0400

    Update ChestMania.js
    - refactor, made it cleaner

--- d2bs/kolbot/libs/scripts/ChestMania.js
+++ d2bs/kolbot/libs/scripts/ChestMania.js
@@ -7,28 +7,26 @@
 
 // todo - if we have run ghostsbusters before this then some of these areas don't need to be re-run
 
-function ChestMania() {
+function ChestMania () {
   Town.doChores();
 
-  for (let prop in Config.ChestMania) {
-    if (Config.ChestMania.hasOwnProperty(prop)) {
-      for (let i = 0; i < Config.ChestMania[prop].length; i += 1) {
-        const nextArea = Config.ChestMania[prop][i];
+  Object.values(Config.ChestMania)
+    .forEach(act => {
+      for (let area of act) {
         if ([
           sdk.areas.BloodMoor, sdk.areas.RockyWaste,
           sdk.areas.SpiderForest, sdk.areas.OuterSteppes, sdk.areas.BloodyFoothills
-        ].includes(nextArea)) {
+        ].includes(area)) {
           // if we precast as soon as we step out of town it sometimes crashes - so do precast somewhere else first
           Precast.doRandomPrecast(false);
         }
-        Pather.journeyTo(Config.ChestMania[prop][i]);
+        Pather.journeyTo(area);
         Precast.doPrecast(false);
-        Misc.openChestsInArea(Config.ChestMania[prop][i]);
+        Misc.openChestsInArea(area);
       }
 
       Town.doChores();
-    }
-  }
+    });
 
   return true;
 }
