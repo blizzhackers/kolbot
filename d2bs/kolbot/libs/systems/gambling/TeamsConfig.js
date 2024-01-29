14cc99e02898bf90662c03cbb7025dda30f9a216
theBGuy <60308670+theBGuy@users.noreply.github.com>
2024-01-29 14:20:39 -0500

    [Formatting] gambling\TeamsConfig.js
    - Not sure how I missed this but convert tabs -> spaces

--- d2bs/kolbot/libs/systems/gambling/TeamsConfig.js
+++ d2bs/kolbot/libs/systems/gambling/TeamsConfig.js
@@ -6,32 +6,32 @@
 */
 
 (function (module) {
-	module.exports = {
-		/**
-			Setting up:
+  module.exports = {
+    /**
+      Setting up:
 
-			"Gamble Team 1": { // Put a unique team name here.
+      "Gamble Team 1": { // Put a unique team name here.
 
-				goldFinders: ["GF Profile 1", "GF Profile 2"], // List of gold finder PROFILE names. They will join gamble games to drop gold
+        goldFinders: ["GF Profile 1", "GF Profile 2"], // List of gold finder PROFILE names. They will join gamble games to drop gold
 
-				gamblers: ["Gambler 1", "Gambler 2"], // List of gambler PROFILE names. They will keep gambling and picking up gold from gold finders.
+        gamblers: ["Gambler 1", "Gambler 2"], // List of gambler PROFILE names. They will keep gambling and picking up gold from gold finders.
 
-				gambleGames: ["Gambling-", "HeyIGamble-"], // Games that gold finders will join, don't use numbers.
+        gambleGames: ["Gambling-", "HeyIGamble-"], // Games that gold finders will join, don't use numbers.
 
-				goldTrigger: 2500000, // Minimum amount of gold before giving it to gamblers.
+        goldTrigger: 2500000, // Minimum amount of gold before giving it to gamblers.
 
-				goldReserve: 200000 // Amount of gold to keep after dropping.
-			}
+        goldReserve: 200000 // Amount of gold to keep after dropping.
+      }
 
-			Once set up properly, the gold finders will run their own games and join gamblers' games when they're out of gold.
-		*/
-		"Gamble Team 1": {
-			goldFinders: [""],
-			gamblers: [""],
-			gambleGames: [""],
+      Once set up properly, the gold finders will run their own games and join gamblers' games when they're out of gold.
+    */
+    "Gamble Team 1": {
+      goldFinders: [""],
+      gamblers: [""],
+      gambleGames: [""],
 
-			goldTrigger: 2500000,
-			goldReserve: 200000
-		},
-	};
+      goldTrigger: 2500000,
+      goldReserve: 200000
+    },
+  };
 })(module);
