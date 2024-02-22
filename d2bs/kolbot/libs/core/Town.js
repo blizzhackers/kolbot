d52fb1aa140d58ceac51a277ebf8eabd55327ed5
SPCommY <159726140+SPCommY@users.noreply.github.com>
2024-02-22 19:24:46 +0200

    Update Town.js
    fix

--- d2bs/kolbot/libs/core/Town.js
+++ d2bs/kolbot/libs/core/Town.js
@@ -1435,11 +1435,11 @@ const Town = {
         : { x: 191, y: 400}; // 640x480
       !getUIFlag(sdk.uiflags.Stash) && Town.openStash();
       say("/swappage");
-			delay(50);
-			say("/insertpage");
+      delay(50);
+      say("/insertpage");
       console.log("Inserted new empty page into stash");
       delay(250);
-      sendClick(firstPageBtn.x, firstPageBtn.y); //click previous page sendClick(firstPageBtn.x, firstPageBtn.y)
+      sendClick(firstPageBtn.x, firstPageBtn.y); //click previous page
       return true
     }
     /**
