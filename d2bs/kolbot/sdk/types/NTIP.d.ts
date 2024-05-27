f02a29f02397771462cd12c617b0ccfba5fe14f9
theBGuy <60308670+theBGuy@users.noreply.github.com>
2024-05-27 12:00:55 -0400

    Update NTIP.d.ts

--- d2bs/kolbot/sdk/types/NTIP.d.ts
+++ d2bs/kolbot/sdk/types/NTIP.d.ts
@@ -1,7 +1,25 @@
 export {};
 declare global {
   namespace NTIP {
-    function OpenFile(filepath: string, notify: boolean): void;
-    function CheckItem(unit: Unit, entryList?: [] | false, verbose?: boolean): void;
+    function addLine(itemString: string, fileName: string): boolean;
+    function OpenFile(filepath: string, notify: boolean): boolean;
+    function CheckQuantityOwned(item_type: (item: ItemUnit) => boolean, item_stats: (item: ItemUnit) => boolean): number;
+    function Clear(): void;
+    function generateTierFunc(tierType: string): (item: ItemUnit) => number;
+    function GetTier(item: ItemUnit): number;
+    function GetMercTier(item: ItemUnit): number;
+    function IsSyntaxInt(ch: string): boolean;
+    const parseAliasIn: {
+      in: string;
+      notin: string;
+      _regex: RegExp;
+      test: (input: string) => boolean;
+      convert: (input: string) => string;
+    };
+    const _props: Map<string, string>;
+    const _aliases: Map<string, string>;
+    const _lists: Map<string, Record<string, number | string>>;
+    function ParseLineInt(input: string, info: any): boolean;
+    function CheckItem(item: ItemUnit, entryList?: [] | false, verbose?: boolean): number | { line: string, result: number };
   }
 }
