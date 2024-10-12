c59b21660bfb32a0f910f31df09d7f63bb251a54
theBGuy <60308670+theBGuy@users.noreply.github.com>
2024-10-12 17:33:34 -0400

    Update DeveloperMode.js
    - Added option to create a character config if one wasn't found
    - Added utility method `log`
    - Added jsdoc
    - Fixed thrown errors from incorrect script names breaking the event handler and handle case sensitivity as it's annoyoing knowing we are trying to run `Baal` but typing `baal` and it failing

--- d2bs/kolbot/libs/scripts/DeveloperMode.js
+++ d2bs/kolbot/libs/scripts/DeveloperMode.js
@@ -7,8 +7,31 @@
 
 const DeveloperMode = new Runnable(
   function DeveloperMode () {
+    const className = sdk.player.class.nameOf(me.classid);
+    
+    /**
+     * @param {string} str 
+     * @param {boolean} [toConsole=false] 
+     * @param {number | string} [color=0] 
+     */
+    const log = function (str = "", toConsole = false, color = 0) {
+      console.log("ÿc8Dev Modeÿc0: " + str);
+      me.overhead(str);
+
+      if (toConsole && typeof color === "string") {
+        color = color.capitalize(true);
+        color = !!sdk.colors.D2Bot[color] ? sdk.colors.D2Bot[color] : 0;
+      }
+      toConsole && D2Bot.printToConsole("Dev Mode :: " + str, color);
+    };
+
     let [done, action, command, userAddon, test] = [false, false, false, false, false];
     let [watchSent, watchRecv, blockSent, blockRecv] = [[], [], [], []];
+    
+    /**
+     * @param {string} msg 
+     * @returns {void}
+     */
     const runCommand = function (msg) {
       if (msg.length <= 1) return;
 
@@ -17,8 +40,7 @@ const DeveloperMode = new Runnable(
 
       switch (cmd.toLowerCase()) {
       case "me":
-        print("Character Level: " + me.charlvl + " | Area: " + me.area + " | x: " + me.x + ", y: " + me.y);
-        me.overhead("Character Level: " + me.charlvl + " | Area: " + me.area + " | x: " + me.x + ", y: " + me.y);
+        log("Character Level: " + me.charlvl + " | Area: " + me.area + " | x: " + me.x + ", y: " + me.y);
 
         break;
       case "useraddon":
@@ -28,7 +50,7 @@ const DeveloperMode = new Runnable(
         break;
       case "run":
         if (msgList.length < 2) {
-          print("ÿc1Missing arguments");
+          console.log("ÿc1Missing arguments");
         } else {
           action = msgList[1];
         }
@@ -44,7 +66,7 @@ const DeveloperMode = new Runnable(
         break;
       case "command":
         if (msgList.length < 2) {
-          print("ÿc1Missing arguments");
+          console.log("ÿc1Missing arguments");
         } else {
           command = msgList.splice(1).join(" ");
         }
@@ -52,33 +74,33 @@ const DeveloperMode = new Runnable(
         break;
       case "watch":
         if (msgList.length < 3) {
-          print("ÿc1Missing arguments");
+          console.log("ÿc1Missing arguments");
           break;
         }
 
         switch (msgList[1].toLowerCase()) {
         case "sent":
           if (msgList[2] === "list") {
-            print("Watching sent packets : ÿc8" + watchSent.join(", "));
+            console.log("Watching sent packets : ÿc8" + watchSent.join(", "));
             break;
           }
 
           watchSent.push(msgList[2]);
-          print("Added ÿc80x" + msgList[2] + "ÿc0 (sent) to watch list");
+          console.log("Added ÿc80x" + msgList[2] + "ÿc0 (sent) to watch list");
           break;
 
         case "recv":
           if (msgList[2] === "list") {
-            print("Watching received packets : ÿc8" + watchRecv.join(", "));
+            console.log("Watching received packets : ÿc8" + watchRecv.join(", "));
             break;
           }
 
           watchRecv.push(msgList[2]);
-          print("Added ÿc80x" + msgList[2] + "ÿc0 (recv) to watch list");
+          console.log("Added ÿc80x" + msgList[2] + "ÿc0 (recv) to watch list");
           break;
 
         default:
-          print("ÿc1Invalid argument : " + msgList[1]);
+          console.log("ÿc1Invalid argument : " + msgList[1]);
           break;
         }
 
@@ -86,23 +108,23 @@ const DeveloperMode = new Runnable(
 
       case "!watch":
         if (msgList.length < 3) {
-          print("ÿc1Missing arguments");
+          console.log("ÿc1Missing arguments");
           break;
         }
 
         switch (msgList[1].toLowerCase()) {
         case "sent":
           if (watchSent.indexOf(msgList[2]) > -1) watchSent.splice(watchSent.indexOf(msgList[2]), 1);
-          print("Removed packet ÿc80x" + msgList[2] + "ÿc0 (sent) from watch list");
+          console.log("Removed packet ÿc80x" + msgList[2] + "ÿc0 (sent) from watch list");
           break;
 
         case "recv":
           if (watchRecv.indexOf(msgList[2]) > -1) watchRecv.splice(watchRecv.indexOf(msgList[2]), 1);
-          print("Removed packet ÿc80x" + msgList[2] + "ÿc0 (recv) from watch list");
+          console.log("Removed packet ÿc80x" + msgList[2] + "ÿc0 (recv) from watch list");
           break;
 
         default:
-          print("ÿc1Invalid argument : " + msgList[1]);
+          console.log("ÿc1Invalid argument : " + msgList[1]);
           break;
         }
 
@@ -110,33 +132,33 @@ const DeveloperMode = new Runnable(
 
       case "block":
         if (msgList.length < 3) {
-          print("ÿc1Missing arguments");
+          console.log("ÿc1Missing arguments");
           break;
         }
 
         switch (msgList[1].toLowerCase()) {
         case "sent":
           if (msgList[2] === "list") {
-            print("Blocking sent packets : ÿc8" + blockSent.join(", "));
+            console.log("Blocking sent packets : ÿc8" + blockSent.join(", "));
             break;
           }
 
           blockSent.push(msgList[2]);
-          print("Added ÿc80x" + msgList[2] + "ÿc0 (sent) to block list");
+          console.log("Added ÿc80x" + msgList[2] + "ÿc0 (sent) to block list");
           break;
 
         case "recv":
           if (msgList[2] === "list") {
-            print("Blocking received packets : ÿc8" + blockRecv.join(", "));
+            console.log("Blocking received packets : ÿc8" + blockRecv.join(", "));
             break;
           }
 
           blockRecv.push(msgList[2]);
-          print("Added ÿc80x" + msgList[2] + "ÿc0 (recv) to block list");
+          console.log("Added ÿc80x" + msgList[2] + "ÿc0 (recv) to block list");
           break;
 
         default:
-          print("ÿc1Invalid argument : " + msgList[1]);
+          console.log("ÿc1Invalid argument : " + msgList[1]);
           break;
         }
 
@@ -144,23 +166,23 @@ const DeveloperMode = new Runnable(
 
       case "!block":
         if (msgList.length < 3) {
-          print("ÿc1Missing arguments");
+          console.log("ÿc1Missing arguments");
           break;
         }
 
         switch (msgList[1].toLowerCase()) {
         case "sent":
           if (blockSent.indexOf(msgList[2]) > -1) blockSent.splice(blockSent.indexOf(msgList[2]), 1);
-          print("Removed packet ÿc80x" + msgList[2] + "ÿc0 (sent) from block list");
+          console.log("Removed packet ÿc80x" + msgList[2] + "ÿc0 (sent) from block list");
           break;
 
         case "recv":
           if (blockRecv.indexOf(msgList[2]) > -1) blockRecv.splice(blockRecv.indexOf(msgList[2]), 1);
-          print("Removed packet ÿc80x" + msgList[2] + "ÿc0 (recv) from block list");
+          console.log("Removed packet ÿc80x" + msgList[2] + "ÿc0 (recv) from block list");
           break;
 
         default:
-          print("ÿc1Invalid argument : " + msgList[1]);
+          console.log("ÿc1Invalid argument : " + msgList[1]);
           break;
         }
 
@@ -218,6 +240,20 @@ const DeveloperMode = new Runnable(
       return false;
     };
 
+    /**
+     * @param {number} key 
+     */
+    const keyEvent = function (key) {
+      switch (key) {
+      case sdk.keys.Spacebar:
+        FileTools.copy("libs/config/" + className + ".js", "libs/config/" + className + "." + me.name + ".js");
+        log("libs/config/" + className + "." + me.name + ".js has been created.", true);
+        log("Please configure your bot and reload or restart", true);
+
+        break;
+      }
+    };
+
     const copiedConfig = copyObj(Config);
     const UnitInfo = new (require("../modules/UnitInfo"));
 
@@ -226,22 +262,38 @@ const DeveloperMode = new Runnable(
       me.overhead("Started developer mode");
       addEventListener("gamepacketsent", packetSent);
       addEventListener("gamepacket", packetReceived);
+
+      if (!FileTools.exists("libs/config/" + className + "." + me.name + ".js")) {
+        console.log("ÿc4UserAddonÿc0: Press HOME and then press SPACE if you want to create character config.");
+        addEventListener("keyup", keyEvent);
+      }
       Config.Silence = false;
 
       while (!done) {
         if (action) {
-          includeIfNotIncluded("scripts/" + action + ".js");
+          try {
+            let script = Loader.fileList.find(function (el) {
+              return String.isEqual(el, action);
+            });
 
-          UnitInfo.check();
-
-          if (isIncluded("scripts/" + action + ".js")) {
-            try {
-              Loader.runScript(action);
-            } catch (e) {
-              console.error(e);
+            if (!script) {
+              throw new Error("Failed to find script: " + action);
             }
-          } else {
-            console.warn("Failed to include: " + action);
+            includeIfNotIncluded("scripts/" + script + ".js");
+
+            UnitInfo.check();
+
+            if (isIncluded("scripts/" + script + ".js")) {
+              try {
+                Loader.runScript(script);
+              } catch (e) {
+                console.error(e);
+              }
+            } else {
+              console.warn("Failed to include: " + script);
+            }
+          } catch (e) {
+            console.error(e);
           }
 
           me.overhead("Done with action");
@@ -273,6 +325,7 @@ const DeveloperMode = new Runnable(
         delay(100);
       }
     } finally {
+      removeEventListener("keyup", keyEvent);
       removeEventListener("gamepacketsent", packetSent);
       removeEventListener("gamepacket", packetReceived);
       Config = copiedConfig;
